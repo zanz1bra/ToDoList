fd58d992965ab60872eab66e88f6a3ca2cc3586b
Ērika Tālberga <103637112+zanz1bra@users.noreply.github.com>
2023-11-06 14:23:23 +0200

    adding second view

--- ToDoList/AppDelegate/AppDelegate.swift
+++ ToDoList/AppDelegate/AppDelegate.swift
@@ -11,16 +11,9 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        
-        let toDoTableViewController = ToDoTableViewController()
-        let navigationController = UINavigationController(rootViewController: toDoTableViewController)
-        window?.rootViewController = navigationController
         
         return true
     }
